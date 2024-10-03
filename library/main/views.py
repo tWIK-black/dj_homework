e6ea28b42db7a1d497df70001a58936f076887db
michael <mixatop678@gmail.com.com>
2024-10-03 12:18:19 +0300

    hm_done

---
+++ library/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
